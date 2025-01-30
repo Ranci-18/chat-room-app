823bf7283e486cdebb290ae1cc1e620dfa3f2ec5
Frank <wanyoike39@gmail.com>
2025-01-30 16:55:13 +0300

    add logic to check 404, send file and server static files to serverjs

--- server.js
+++ server.js
@@ -4,3 +4,35 @@ import path from 'path';
 import mime from 'mime';
 
 let cache = {};
+
+function send404(response) {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('Error 404: resource not found.');
+    response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+    response.writeHead(200, {'Content-Type': mime.lookup(path.basename(filePath))});
+    response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+    if (cache[absPath]) {
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function(exists) {
+            if (exists) {
+                fs.readFile(absPath, function(err, data) {
+                    if (err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                });
+            } else {
+                send404(response);
+            }
+        });
+    }
+}
